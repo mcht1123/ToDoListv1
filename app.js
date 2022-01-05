a2bc432fcf991eeffaf56c3b3b175213b4f6bd98
CHT <86104407+mcht1123@users.noreply.github.com>
2022-01-05 11:02:41 +0800

    remove redundancies

--- app.js
+++ app.js
@@ -25,12 +25,6 @@ app.get("/work", function(req,res){
     res.render("list",{kindOfDay: "Work List", newListItem: workItems});
 })
 
-app.post("/work",function(req,res){
-    let item = req.body.newItem
-    workItems.push(item);
-    res.redirect("/work");
-})
-
 app.post("/",function(req,res){
     let item = req.body.newItem;
 
